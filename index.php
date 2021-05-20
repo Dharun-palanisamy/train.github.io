b4a71b7750c5471e966002893cf08d6a347aa717
Dharun <63730630+Dharun-palanisamy@users.noreply.github.com>
2021-05-20 17:24:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,20 @@
 <body>
 <?php
     echo "hello";
-     $conn = pg_connect(getenv("postgres://mjthlopbqtqvkd:a483b85042040a4413668e4212b89932b6276a2a716ed1e3d7b376dc7d33e758@ec2-52-21-252-142.compute-1.amazonaws.com:5432/df56u1c1jatv1b"));
-    if (!$conn) 
-   {
-     echo "Database connection failed.";
-   }
-   else 
-   {
-     echo "Database connection success.";
-   }
-?>123<a href="public/Train Ticket Reservation/login.php"  target="_blank">click here</a>
+    $db = pg_connect(getenv("postgres://mjthlopbqtqvkd:a483b85042040a4413668e4212b89932b6276a2a716ed1e3d7b376dc7d33e758@ec2-52-21-252-142.compute-1.amazonaws.com:5432/df56u1c1jatv1b"));
+    $conn = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+    if (!$conn) {
+        echo "Database connection failed.";
+    } else {
+        echo "Database connection success.";
+    }
+?><a href="public/Train Ticket Reservation/login.php"  target="_blank">click here</a>
 </body>
 </html>
